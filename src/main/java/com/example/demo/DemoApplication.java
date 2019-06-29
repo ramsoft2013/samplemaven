73d9baf0ad8bfc9e6528327a09630f9f4085c837
Naveenprabhu.B.E <naveen@Apples-MacBook-Pro.local>
2019-06-29 23:15:40 +0530

    syso added

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		
+	System.out.println("print");
 		SpringApplication.run(DemoApplication.class, args);
+		
 	}
 
 }
